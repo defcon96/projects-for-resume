b0245778937832a7db15f5eef0a6deaf9b890702
Anthony <slayer0412@gmail.com>
2021-07-11 14:26:15 -0400

    a database

---
+++ greenwoodcem.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('employee.db')
+
+c = conn.cursor()
+
+c.excute("""CREATE TABLE employee(
+        first text,
+        last text,
+        pay integer
+        )""")
+
+conn.commit()
+conn.close()
