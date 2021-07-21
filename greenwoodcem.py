dfae31f8a3c5c1e344c03fcc655976978c0359a9
Anthony <slayer0412@gmail.com>
2021-07-21 10:30:13 -0400

    earsed some changes

--- greenwoodcem.py
+++ greenwoodcem.py
@@ -3,12 +3,3 @@ import sqlite3
 conn = sqlite3.connect('employee.db')
 
 c = conn.cursor()
-
-c.excute("""CREATE TABLE employee (
-            first text,
-            last text,
-            pay integer
-        )""")
-
-conn.commit()
-conn.close()
